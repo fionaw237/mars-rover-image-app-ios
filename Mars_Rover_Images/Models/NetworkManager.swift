5dd9fc078141cf4ae783e0f9309660d562c0b8f1
Fiona Wilson <fiona.wilson@resdiary.com>
2023-09-26 14:50:46 +0100

    Updated data fetching to use async await syntax

--- Mars_Rover_Images/Models/NetworkManager.swift
+++ Mars_Rover_Images/Models/NetworkManager.swift
@@ -9,38 +9,46 @@
 import Foundation
 import CoreData
 
+enum DataFetchError: String, Error {
+    case urlCreation = "Could not create URL."
+    case coreDataSave = "Error saving to Core Data."
+    case decode = "Error decoding JSON."
+    case server = "The server responded with an error."
+}
+
 struct NetworkManager {
-    
-    func fetchData(sol: Int, rover: String, context: NSManagedObjectContext, completion: @escaping (Result<[Photo], Error>) -> Void) {
-        
+    func fetchData(
+        sol: Int,
+        rover: String,
+        context: NSManagedObjectContext
+    ) async throws -> [Photo] {
         let baseUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/"
         let key = "SosAw8PH06hY4UgdReSvYk0F0GfqFHPv0V9GWFK8"
         
         guard let url = URL(string: "\(baseUrl)\(rover)/photos?sol=\(sol)&api_key=\(key)") else {
-            return
+            throw DataFetchError.urlCreation
+        }
+
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            throw DataFetchError.server
         }
         
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            DispatchQueue.main.async {
-                if error == nil {
-                    guard let data = data else {return}
-                    let decoder = JSONDecoder()
-                    decoder.userInfo[CodingUserInfoKey.context!] = context
-                    do {
-                        let photosResponse = try decoder.decode(PhotoData.self, from: data)
-                        do {
-                            try context.save()
-                        } catch {
-                            print("Error saving to core data for sol \(sol)")
-                        }
-                        completion(.success(photosResponse.photos))
-                    } catch let jsonError {
-                        completion(.failure(jsonError))
-                    }
-                } else {
-                    completion(.failure(error!))
-                }
+        let decoder = JSONDecoder()
+        decoder.userInfo[CodingUserInfoKey.context!] = context
+        
+        do {
+            let photosResponse = try decoder.decode(PhotoData.self, from: data)
+            do {
+                try context.save()
+            } catch {
+                throw DataFetchError.coreDataSave
             }
-        }.resume()
+            return photosResponse.photos
+        } catch {
+            throw DataFetchError.decode
+        }
+
     }
 }
